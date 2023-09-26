937c43c9765b1404c4c4bbf519d2a93b1f0d1c50
rlaeogus0911 <rlaeogus0911@nate.com>
2023-09-26 14:42:42 +0900

    [생성 HashMapTest]
    사전검색 테스트(HashMap, Set)

---
+++ javaTest/src/ch07/sec04/HashMapTest.java
@@ -0,0 +1,38 @@
+package ch07.sec04;
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
+public class HashMapTest {
+	public static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		HashMap<String, String> hmDic = new HashMap<String, String>();
+		
+		//HashMap 추가(삽입)
+		hmDic.put("baby", "아기");
+		hmDic.put("love", "사랑");
+		hmDic.put("apple", "사과");
+		
+		//HashMap 출력(키객체->Set)
+		Set<String> keyset = hmDic.keySet();
+		for(String key : keyset) {
+			System.out.printf("key=%s \t value=%s \n",key, hmDic.get(key));
+		}
+		
+		//검색기능
+		while(true) {
+			System.out.print("검색단어(종료:exit)>>");
+			String search = scan.nextLine().trim(); 
+			if(search.equals("exit") || search.equals("EXIT")) {
+				System.out.println("단어검색을 종료합니다.");
+				break;
+			}  
+			System.out.printf("%s >> %s \n", search, hmDic.get(search));
+		}
+		
+		System.out.println("The end");
+
+	}
+}
